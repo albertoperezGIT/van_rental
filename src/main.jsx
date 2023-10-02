0aedb5a62bd6680d09657b4a5634e028671b4185
albertoperezGIT <albertop98@hotmail.com>
2023-10-01 23:17:22 -0400

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import App from './pages/Home.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Vans from './pages/Vans/Vans.jsx'
+import VanDetail from './pages/Vans/VanDetail.jsx'
+import Dashboard from './pages/Host/Dashboard.jsx'
+import Income from './pages/Host/income.jsx'
+import Reviews from './pages/Host/reviews.jsx'
+import Layout from './components/Layout.jsx'
+import HostLayout from './components/HostLayout.jsx'
+import './server.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <header>
-        <Link to='/'>#VANLIFE</Link>
-          <nav>
-            <Link to='/About'>About</Link>
-          </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+           
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
